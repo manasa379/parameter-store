0e7883674c2b1e9415963f5b73380c793a9cff40
DELL <manasajalagam379@gmail.com>
2024-05-04 18:41:38 +0530

    expense terraform

--- main.tf
+++ main.tf
@@ -7,6 +7,8 @@ resource "aws_ssm_parameter" "params" {
 variable "parameters" {
   default = [
     {name = "prod.rds.master_password",value ="expenseprod1234",type = "SecureString"},
-    {name =  "prod.rds.master_username",value = "expense", type = "String"}
+    {name =  "prod.rds.master_username",value = "expense", type = "String"},
+    {name =  "prod.expense.frontend.backend_url",value = "http://backend-prod.devops155.online/", type = "String"},
+    {name =  "dev.rds.endpoint",value = "prod-mysql.cluster-clmsay24iffv.us-east-1.rds.amazonaws.com",type = "String"}
   ]
 }
